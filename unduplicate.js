1e74d0f1453003bcd4c766e26e837586b08864a5
Benjamin Alan Alexander <baa2165@.columbia.edu>
2023-08-21 15:49:52 -0400

    initial commit

---
+++ unduplicate.js
@@ -0,0 +1,34 @@
+  (function ($) {
+    // Find duplicate IDs
+    function findAndFixDuplicateIds() {
+        var idMap = {};
+        var duplicateIds = [];
+    
+        // Iterate through all elements
+        $('[id]').each(function () {
+        var id = this.id;
+    
+        // Check if the ID already exists in the map
+        if (idMap[id] === undefined) {
+            idMap[id] = true;
+        } else {
+            duplicateIds.push(id);
+        }
+        });
+    
+        // Fix duplicate IDs by appending a unique suffix
+        for (var i = 0; i < duplicateIds.length; i++) {
+        var duplicateId = duplicateIds[i];
+        $('[id="' + duplicateId + '"]').each(function (index) {
+            if (index > 0) {
+            $(this).attr('id', duplicateId + '_fix' + index);
+            }
+        });
+        }
+    }
+    $(document).ready(function () {
+      console.log('Custom script is running.');
+      findAndFixDuplicateIds();
+      console.log("yeet");
+    });
+  })(jQuery);
